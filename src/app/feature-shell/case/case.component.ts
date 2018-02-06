d56ce8e0b22a7570c81b4e9842832babfe3758c4
shubhgupta276 <shubham.gupta1@globallogic.com>
2018-02-06 20:22:20 +0530

    intermitent commit

--- src/app/feature-shell/case/case.component.ts
+++ src/app/feature-shell/case/case.component.ts
@@ -1,68 +1,220 @@
-import { Component, OnInit } from '@angular/core';
-import { debuglog } from 'util';
+import { Component, OnInit } from "@angular/core";
+import { debuglog } from "util";
 declare var $;
 
 @Component({
-  selector: 'app-case',
-  templateUrl: './case.component.html',
-  styleUrls: ['./case.component.css']
+  selector: "app-case",
+  templateUrl: "./case.component.html",
+  styleUrls: ["./case.component.css"]
 })
 export class CaseComponent implements OnInit {
-jsonRunning:any;
-jsonCompleted:any;
+  jsonRunning: any;
+  jsonCompleted: any;
   constructor() {
-    this.jsonRunning= [
-      {Id:1,CaseId:'I/DRT/3',CourtCaseID:'',CustomerName:'Anup',RecourceType:'DRT',CaseStage:'FOR VALUATION OF ASSET',NextHearingDate:'22-Dec-2017',Branch:'Delhi',Employee:'Anup'  },
-      {Id:2,CaseId:'I/LKAD/4',CourtCaseID:'',CustomerName:'Puneet',RecourceType:'LOK_ADALATH',CaseStage:'FOR SALE PERMISSION OF VEHICLE',NextHearingDate:'17-Jan-2018	',Branch:'Delhi',Employee:'Puneet'  },
-      {Id:3,CaseId:'I/LKAD/5',CourtCaseID:'',CustomerName:'Vipin',RecourceType:'LOK_ADALATH',CaseStage:'FOR SALE PERMISSION OF VEHICLE',NextHearingDate:'17-Jan-2018	',Branch:'Delhi',Employee:'Vipin'  },
-      {Id:4,CaseId:'I/LKAD/6',CourtCaseID:'',CustomerName:'Anil',RecourceType:'LOK_ADALATH',CaseStage:'FOR SALE PERMISSION OF VEHICLE',NextHearingDate:'17-Jan-2018	',Branch:'Delhi',Employee:'Anil'  },
-      {Id:5,CaseId:'I/LKAD/7',CourtCaseID:'',CustomerName:'Sourav',RecourceType:'LOK_ADALATH',CaseStage:'FOR SALE PERMISSION OF VEHICLE',NextHearingDate:'17-Jan-2018	',Branch:'Delhi',Employee:'Sourav'  },
-    
-    ];
-    this.jsonCompleted= [
-      {Id:1,CaseId:'I/DRT/3',CourtCaseID:'',FillingDate:'2018-01-18',CompletionDate:'2018-01-19',Branch:'Delhi',Court:'',Manager:'Anup'  },
-      {Id:2,CaseId:'I/LKAD/4',CourtCaseID:'',FillingDate:'2018-01-19',CompletionDate:'2018-01-19',Branch:'Mumbai',Court:'',Manager:'Puneet'  },
-      {Id:3,CaseId:'I/LKAD/5',CourtCaseID:'',FillingDate:'2018-01-21',CompletionDate:'2018-01-21',Branch:'Banglore',Court:'',Manager:'Vipin'  },
-      {Id:4,CaseId:'I/LKAD/6',CourtCaseID:'',FillingDate:'2018-01-20',CompletionDate:'2018-01-20',Branch:'Delhi',Court:'',Manager:'Anil'  },
-      {Id:5,CaseId:'I/LKAD/7',CourtCaseID:'',FillingDate:'2018-01-24',CompletionDate:'2018-01-25',Branch:'Delhi',Court:'',Manager:'Sourav'  },
-    
-    ];
-   }
-   
-   showEditModal(){
-    $('#editCaseModal').modal('show');
-    }
-
-  ngOnInit() {   
-    setTimeout(function()
-  {
-    $('#example2').DataTable({
-      'paging'      : true,
-      'lengthChange': true,
-      'ordering'    : true,
-      'info'        : true,
-      'autoWidth'   : false
-    });
-  },50);
-
-  $($.document).ready(function() {
-    $("#example1").DataTable({
-      paging: true,
-      lengthChange: true,
-      searching: true,
-      ordering: true,
-      info: true,
-      autoWidth: false,
-      lengthMenu: [[10, 15, 25, -1], [10, 15, 25, "All"]],
-      pageLength: 15,
-      oLanguage: {
-        sLengthMenu: "Show _MENU_ rows",
-        sSearch: "",
-        sSearchPlaceholder: "Search..."
+    this.jsonRunning = [
+      {
+        Id: 1,
+        CaseId: "I/DRT/3",
+        CourtCaseID: "",
+        CustomerName: "Anup",
+        RecourceType: "DRT",
+        CaseStage: "FOR VALUATION OF ASSET",
+        NextHearingDate: "22-Dec-2017",
+        Branch: "Delhi",
+        Employee: "Anup"
+      },
+      {
+        Id: 2,
+        CaseId: "I/LKAD/4",
+        CourtCaseID: "",
+        CustomerName: "Puneet",
+        RecourceType: "LOK_ADALATH",
+        CaseStage: "FOR SALE PERMISSION OF VEHICLE",
+        NextHearingDate: "17-Jan-2018	",
+        Branch: "Delhi",
+        Employee: "Puneet"
+      },
+      {
+        Id: 3,
+        CaseId: "I/LKAD/5",
+        CourtCaseID: "",
+        CustomerName: "Vipin",
+        RecourceType: "LOK_ADALATH",
+        CaseStage: "FOR SALE PERMISSION OF VEHICLE",
+        NextHearingDate: "17-Jan-2018	",
+        Branch: "Delhi",
+        Employee: "Vipin"
+      },
+      {
+        Id: 4,
+        CaseId: "I/LKAD/6",
+        CourtCaseID: "",
+        CustomerName: "Anil",
+        RecourceType: "LOK_ADALATH",
+        CaseStage: "FOR SALE PERMISSION OF VEHICLE",
+        NextHearingDate: "17-Jan-2018	",
+        Branch: "Delhi",
+        Employee: "Anil"
+      },
+      {
+        Id: 5,
+        CaseId: "I/LKAD/7",
+        CourtCaseID: "",
+        CustomerName: "Sourav",
+        RecourceType: "LOK_ADALATH",
+        CaseStage: "FOR SALE PERMISSION OF VEHICLE",
+        NextHearingDate: "17-Jan-2018	",
+        Branch: "Delhi",
+        Employee: "Sourav"
       }
-    });
-  });
-   
+    ];
+    this.jsonCompleted = [
+      {
+        Id: 1,
+        CaseId: "I/DRT/3",
+        CourtCaseID: "",
+        FillingDate: "2018-01-18",
+        CompletionDate: "2018-01-19",
+        Branch: "Delhi",
+        Court: "",
+        Manager: "Anup"
+      },
+      {
+        Id: 2,
+        CaseId: "I/LKAD/4",
+        CourtCaseID: "",
+        FillingDate: "2018-01-19",
+        CompletionDate: "2018-01-19",
+        Branch: "Mumbai",
+        Court: "",
+        Manager: "Puneet"
+      },
+      {
+        Id: 3,
+        CaseId: "I/LKAD/5",
+        CourtCaseID: "",
+        FillingDate: "2018-01-21",
+        CompletionDate: "2018-01-21",
+        Branch: "Banglore",
+        Court: "",
+        Manager: "Vipin"
+      },
+      {
+        Id: 4,
+        CaseId: "I/LKAD/6",
+        CourtCaseID: "",
+        FillingDate: "2018-01-20",
+        CompletionDate: "2018-01-20",
+        Branch: "Delhi",
+        Court: "",
+        Manager: "Anil"
+      },
+      {
+        Id: 5,
+        CaseId: "I/LKAD/7",
+        CourtCaseID: "",
+        FillingDate: "2018-01-24",
+        CompletionDate: "2018-01-25",
+        Branch: "Delhi",
+        Court: "",
+        Manager: "Sourav"
+      }
+    ];
   }
 
+  showEditModal() {
+    $("#editCaseModal").modal("show");
+  }
+
+  ngOnInit() {
+    $($.document).ready(function() {
+      var arLengthMenu = [[10, 15, 25, -1], [10, 15, 25, "All"]];
+      var selectedPageLength = 15;
+      const $table = $("#example1").DataTable({
+        lengthMenu: arLengthMenu,
+        pageLength: selectedPageLength,
+        oLanguage: {
+          sLengthMenu: "Show _MENU_ rows",
+          sSearch: "",
+          sSearchPlaceholder: "Search..."
+        },
+        initComplete: function() {
+          var tableid = "example1";
+          var $rowSearching = $("#" + tableid + "_wrapper");
+          $rowSearching.find(".row:eq(0)").hide();
+
+          for (var i = 0; i < arLengthMenu[0].length; i++) {
+            $("#ddlLengthMenu").append(
+              "<option value=" +
+                arLengthMenu[0][i] +
+                ">" +
+                arLengthMenu[1][i] +
+                "</option>"
+            );
+          }
+          $("#ddlLengthMenu").val(selectedPageLength);
+
+          $("#ddlLengthMenu").on("change", function() {
+            $rowSearching
+              .find(".row:eq(0)")
+              .find("select")
+              .val($(this).val())
+              .change();
+          });
+        }
+      });
+      $table.columns().every(function() {
+        $("#txtSearch").on("keyup change", function() {
+          if ($table.search() !== this.value) {
+            $table.search(this.value).draw();
+          }
+        });
+      });
+    });
+
+    $($.document).ready(function() {
+      var arLengthMenu = [[10, 15, 25, -1], [10, 15, 25, "All"]];
+      var selectedPageLength = 15;
+      const $table = $("#example2").DataTable({
+        lengthMenu: arLengthMenu,
+        pageLength: selectedPageLength,
+        oLanguage: {
+          sLengthMenu: "Show _MENU_ rows",
+          sSearch: "",
+          sSearchPlaceholder: "Search..."
+        },
+        initComplete: function() {
+          var tableid = "example2";
+          var $rowSearching = $("#" + tableid + "_wrapper");
+          $rowSearching.find(".row:eq(0)").hide();
+
+          for (var i = 0; i < arLengthMenu[0].length; i++) {
+            $("#ddlLengthMenu2").append(
+              "<option value=" +
+                arLengthMenu[0][i] +
+                ">" +
+                arLengthMenu[1][i] +
+                "</option>"
+            );
+          }
+          $("#ddlLengthMenu2").val(selectedPageLength);
+
+          $("#ddlLengthMenu2").on("change", function() {
+            $rowSearching
+              .find(".row:eq(0)")
+              .find("select")
+              .val($(this).val())
+              .change();
+          });
+        }
+      });
+      $table.columns().every(function() {
+        $("#txtSearch2").on("keyup change", function() {
+          if ($table.search() !== this.value) {
+            $table.search(this.value).draw();
+          }
+        });
+      });
+    });
+  }
 }
--- src/app/feature-shell/user/user.component.ts
+++ src/app/feature-shell/user/user.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { UserRoles, UserStatus, KeyValue } from '../../shared/Utility/util-common';
-import { matchValidator } from '../../shared/Utility/util-custom.validation';
-import { debug } from 'util';
-import { filter } from 'rxjs/operators/filter';
-import { User, Users } from '../../shared/models/user/user';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import {
+  UserRoles,
+  UserStatus,
+  KeyValue
+} from "../../shared/Utility/util-common";
+import { matchValidator } from "../../shared/Utility/util-custom.validation";
+import { debug } from "util";
+import { filter } from "rxjs/operators/filter";
+import { User, Users } from "../../shared/models/user/user";
 declare var $;
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
-
 export class UserComponent implements OnInit {
   arr: User[];
-  usertype = 'All';
-  filterby = 'All';
+  usertype = "All";
+  filterby = "All";
   editForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
@@ -27,105 +35,135 @@ export class UserComponent implements OnInit {
     this.GetAllCustomer();
   }
 
-  showEditModal(){
-    $('#editUserModal').modal('show');
-    }
+  showEditModal() {
+    $("#editUserModal").modal("show");
+  }
 
   GetAllCustomer() {
     this.arr = Users;
-    $($.document).ready(function () {
+    $($.document).ready(function() {
       var arLengthMenu = [[10, 15, 25, -1], [10, 15, 25, "All"]];
       var selectedPageLength = 15;
-      const $table = $('#example1').DataTable({
+      const $table = $("#example1").DataTable({
         columns: [
-          { name: '#', orderable: true },
-          { name: 'Name', orderable: true },
-          { name: 'Email', orderable: true },
-          { name: 'Phone', orderable: true },
-          { name: 'Role', orderable: false },
-          { name: 'Status', orderable: false }
+          { name: "#", orderable: true },
+          { name: "Name", orderable: true },
+          { name: "Email", orderable: true },
+          { name: "Phone", orderable: true },
+          { name: "Role", orderable: false },
+          { name: "Status", orderable: false }
         ],
-        'lengthMenu': arLengthMenu,
-        'pageLength': selectedPageLength,
-        'oLanguage': {
-          'sLengthMenu': 'Show _MENU_ rows',
-          'sSearch': '',
-          'sSearchPlaceholder': 'Search...'
+        lengthMenu: arLengthMenu,
+        pageLength: selectedPageLength,
+        oLanguage: {
+          sLengthMenu: "Show _MENU_ rows",
+          sSearch: "",
+          sSearchPlaceholder: "Search..."
         },
-        initComplete: function () {
+        initComplete: function() {
           var tableid = "example1";
           var $rowSearching = $("#" + tableid + "_wrapper");
           $rowSearching.find(".row:eq(0)").hide();
 
           for (var i = 0; i < arLengthMenu[0].length; i++) {
-            $("#ddlLengthMenu").append("<option value=" + arLengthMenu[0][i] + ">" + arLengthMenu[1][i] + "</option>");
+            $("#ddlLengthMenu").append(
+              "<option value=" +
+                arLengthMenu[0][i] +
+                ">" +
+                arLengthMenu[1][i] +
+                "</option>"
+            );
           }
           $("#ddlLengthMenu").val(selectedPageLength);
 
-          $("#ddlLengthMenu").on("change", function () {
-            $rowSearching.find(".row:eq(0)").find("select").val($(this).val()).change();
+          $("#ddlLengthMenu").on("change", function() {
+            $rowSearching
+              .find(".row:eq(0)")
+              .find("select")
+              .val($(this).val())
+              .change();
           });
         }
       });
 
-      $table.columns().every(function () {
-        $('#txtSearch').on('keyup change', function () {
+      $table.columns().every(function() {
+        $("#txtSearch").on("keyup change", function() {
           if ($table.search() !== this.value) {
             $table.search(this.value).draw();
           }
         });
       });
 
-      $table.columns().every(function () {
+      $table.columns().every(function() {
         // user filter
-        $('#ddlUserFilter').on('change', function () {
-           const status = $(this).val();
-          if (status === 'All') {
+        $("#ddlUserFilter").on("change", function() {
+          const status = $(this).val();
+          if (status === "All") {
             $table
               .columns(4)
-              .search('')
+              .search("")
               .draw();
           } else if ($table.columns(4).search() !== this.value) {
             $table
               .columns(4)
               .search(this.value)
               .draw();
-          } else { }
+          } else {
+          }
         });
         // status filter
-        $('#ddlStatusFilter').on('change', function () {
+        $("#ddlStatusFilter").on("change", function() {
           const status = $(this).val();
-          if (status === 'All') {
+          if (status === "All") {
             $table
               .columns(5)
-              .search('')
+              .search("")
               .draw();
           } else if ($table.columns(5).search() !== this.value) {
             $table
               .columns(5)
               .search(this.value)
               .draw();
-          } else { }
+          } else {
+          }
         });
       });
     });
   }
-  
+
   editUser(user) {
     this.createForm(user);
-    $('#editUserModal').modal('show');
+    $("#editUserModal").modal("show");
   }
 
   createForm(user) {
     this.editForm = this.fb.group({
       firstName: [user == null ? null : user.FirstName, Validators.required],
       lastName: [user == null ? null : user.LastName, Validators.required],
-      organisation: [user == null ? null : user.Organisation, Validators.required],
-      addressLine1: [user == null ? null : user.AddressLine1, Validators.required],
-      addressLine2: [user == null ? null : user.AddressLine2, Validators.required],
-      postalCode: [user == null ? null : user.PostalCode, Validators.compose([Validators.required,  Validators.minLength(4)])],
-      email: [user == null ? null : user.Email, Validators.compose([Validators.required, Validators.email])],
-      mobileNumber: [user == null ? null : user.MobileNumber, Validators.compose([Validators.required, Validators.minLength(10)])],
+      organisation: [
+        user == null ? null : user.Organisation,
+        Validators.required
+      ],
+      addressLine1: [
+        user == null ? null : user.AddressLine1,
+        Validators.required
+      ],
+      addressLine2: [
+        user == null ? null : user.AddressLine2,
+        Validators.required
+      ],
+      postalCode: [
+        user == null ? null : user.PostalCode,
+        Validators.compose([Validators.required, Validators.minLength(4)])
+      ],
+      email: [
+        user == null ? null : user.Email,
+        Validators.compose([Validators.required, Validators.email])
+      ],
+      mobileNumber: [
+        user == null ? null : user.MobileNumber,
+        Validators.compose([Validators.required, Validators.minLength(10)])
+      ],
       role: [user == null ? 1 : user.UserTypeCode],
       status: [user == null ? 1 : user.StatusCode]
     });
