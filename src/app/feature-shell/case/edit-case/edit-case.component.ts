9812f10288229d070cb01a53c53c880aab111192
Ravindra Pratap <ravindra.pratap@synapse.com>
2018-02-07 11:56:02 +0530

    edit form binding and datepicker issue resolved

--- src/app/feature-shell/case/edit-case/edit-case.component.ts
+++ src/app/feature-shell/case/edit-case/edit-case.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import { debuglog } from 'util';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { CaseResource, CaseManager,CaseCourt,CaseState,ParentCase,CaseCustomerName,CaseBranch,CaseStage,CaseEmployee,CaseCourtPlace, KeyValue } from '../../../shared/Utility/util-common';
+import {
+  CaseResource, CaseManager, CaseCourt, CaseState, ParentCase, CaseCustomerName,
+  CaseBranch, CaseStage, CaseEmployee, CaseCourtPlace, KeyValue
+} from '../../../shared/Utility/util-common';
 import { matchValidator } from '../../../shared/Utility/util-custom.validation';
 
 declare var $;
@@ -9,13 +12,12 @@ declare var $;
 
 @Component({
   selector: 'app-edit-case',
-  templateUrl:'../edit-case/edit-case.component.html'
- //template:`<h1>test popup</h1>`
+  templateUrl: '../edit-case/edit-case.component.html'
+  // template:`<h1>test popup</h1>`
 })
-export class EditCaseComponent implements OnInit
-{
+export class EditCaseComponent implements OnInit {
 
-  editCaseForm: FormGroup;
+  @Input() editCaseForm: FormGroup;
 
   Resource: KeyValue[] = CaseResource;
   Manager: KeyValue[] = CaseManager;
@@ -29,57 +31,19 @@ export class EditCaseComponent implements OnInit
   CourtPlace: KeyValue[] = CaseCourtPlace;
   // emailValidationMessage: string = "Email address is required.";
 
-  EditCaseUser() {
-    this.editCaseForm = this.fb.group({
-      compliance: [],
-      caseId: [null, Validators.required],
-      courtCaseId: [],
-      recourse: [1],
-      manager: [1],
-      court: [1],
-      state: [1],
-      parentCase: [1],
-      nextHearingDate: [],
-      customerName: [1],
-      remark: [null, Validators.required],
-      groundforclosingfile: [],
-      disposedoffFileNo: [],
-      branch: [1],
-      filingdate: [],
-      stage: [1],
-      employee: [1],
-      courtplace: [1],
-      oppLawyer: [],
-      childCase: [],
-      lastHearingDate: [],
-      uploadDocument: [],
-      completionDate: []
-    });
-  }
-
-  constructor(private fb: FormBuilder) {
-    this.EditCaseUser();
-  }
-
   submitEditCaseUser(data) {
-    debugger;
-    $.toaster({ priority : 'success', title : 'Success', message : 'Case updated successfully'});
+    $.toaster({ priority: 'success', title: 'Success', message: 'Case updated successfully' });
   }
 
 
-ngOnInit()
-  {
-    // this.editCaseForm.get('email').valueChanges.subscribe(
-    //   (e) => {
-    //     if (e != "") {
-    //       this.editCaseForm.get('email').setValidators([Validators.email]);
-    //       this.emailValidationMessage = "Email format is not correct.";
-    //     } else {
-    //       this.editCaseForm.get('email').setValidators([Validators.required]);
-    //       this.emailValidationMessage = "Email address is required.";
-    //     }
-    //   }
-    // )
-
+  ngOnInit() {
+    const self = this;
+    $(document).ready(function () {
+      $('.input-group.date').datepicker().on('changeDate', function (ev) {
+        const attrName = $(this).find('input').attr('formControlName');
+        const attrValue = $(this).find('input').val();
+        self.editCaseForm.controls[attrName].setValue(attrValue);
+      });
+    });
   }
 }
