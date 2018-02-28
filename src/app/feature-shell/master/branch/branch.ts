5802a7322059721495978400dcdb84d10a3014b5
Vipin Singh1 <vipin.singh1@synapse.com>
2018-02-28 11:46:36 +0530

    api-intergration

---
+++ src/app/feature-shell/master/branch/branch.ts
@@ -0,0 +1,3 @@
+export class Branch {
+    id: number;
+}
--- src/app/feature-shell/master/master.config.ts
+++ src/app/feature-shell/master/master.config.ts
@@ -27,3 +27,7 @@ export let updateStageUrl = "master/update/stage";
 export let addComplianceUrl = "master/add/compliance";
 export let getCompliancesUrl = "master/compliances";
 export let updateComplianceUrl = "master/update/compliance";
+
+export let addBranchUrl = "master/add/branch";
+export let getBranchesUrl = "master/branches";
+export let updateBranchUrl = "master/update/branch";
