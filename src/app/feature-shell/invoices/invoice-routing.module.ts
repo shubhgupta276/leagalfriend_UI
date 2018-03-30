ffb6cb40a8fb38fc5b3277dcd4f4c2108be09805
Anupd844 <anup.dubey1@globallogic.com>
2018-03-30 17:06:43 +0530

    Invoice page

--- src/app/feature-shell/invoices/invoice-routing.module.ts
+++ src/app/feature-shell/invoices/invoice-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InvoiceComponent } from './invoice.component';
-
+import { InvoiceFormComponent } from './invoice-form/invoiceform-component';
 const caseRoutes: Routes = [
-    { path: '', component: InvoiceComponent, pathMatch: 'full'}
-    ];
+  { path: '', component: InvoiceComponent, pathMatch: 'full' },
+  { path: 'invoiceform', component: InvoiceFormComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(caseRoutes)],
   exports: [RouterModule]
 })
-export class InvoiceRoutingModule {}
+export class InvoiceRoutingModule { }
