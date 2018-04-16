8d80e9778948aa6ad6e2af7f319684d6526a8c2e
shubhgupta276 <shubham.gupta1@globallogic.com>
2018-04-16 15:48:22 +0530

    invoice template form complete

--- src/app/feature-shell/master/invoice-info/invoice-info.component.ts
+++ src/app/feature-shell/master/invoice-info/invoice-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MasterService } from '../master.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-invoice-info',
@@ -9,6 +10,7 @@ import { MasterService } from '../master.service';
 })
 
 export class InvoiceInfoComponent implements OnInit {
+    submitted = false;
     termsCondition: any;
     address: any;
     logo: any;
@@ -16,19 +18,17 @@ export class InvoiceInfoComponent implements OnInit {
     isShowUpload = true;
     invoiceForm: FormGroup;
     editTemplate = false;
-    constructor(private fb: FormBuilder, private masterService: MasterService) {
+    constructor(private fb: FormBuilder, private masterService: MasterService, public sanitizer: DomSanitizer) {}
+    ngOnInit() {
         this.getTemplateData();
         this.createForm();
-    }
-    ngOnInit() {
-        //
     }
     getTemplateData() {
         const client_id = localStorage.getItem('client_id');
         this.masterService.getInvoiceData(client_id).subscribe(
             result => {
                 const templateData = result;
-                this.photoUrl = templateData.invoiceHeader.logo;
+                this.photoUrl = this.sanitizer.bypassSecurityTrustUrl('data:image/png+xml;base64,' + templateData.invoiceHeader.logo);
                 this.address = templateData.invoiceFooter.address;
                 this.termsCondition = templateData.invoiceFooter.termsCondition;
             }
@@ -41,46 +41,66 @@ export class InvoiceInfoComponent implements OnInit {
             city: ['', Validators.required],
             state: ['', Validators.required],
             zipcode: ['', Validators.required],
-            // description: ['', Validators.required],
             terms: ['', Validators.required],
             logo: ['', Validators.required]
         });
+        if (this.address) {
+            this.invoiceForm.get('addressLine1').setValue(this.address.address1);
+            this.invoiceForm.get('addressLine2').setValue(this.address.address2);
+            this.invoiceForm.get('city').setValue(this.address.city);
+            this.invoiceForm.get('state').setValue(this.address.state);
+            this.invoiceForm.get('zipcode').setValue(this.address.zipCode);
+        }
+        if (this.termsCondition) {
+            this.invoiceForm.get('terms').setValue(this.termsCondition);
+        }
+        if (this.photoUrl || this.photoUrl.changingThisBreaksApplicationSecurity) {
+            if (this.photoUrl.changingThisBreaksApplicationSecurity) {
+                this.invoiceForm.get('logo').setValue(this.photoUrl.changingThisBreaksApplicationSecurity.split(',')[1]);
+            }else {
+                this.invoiceForm.get('logo').setValue(this.photoUrl);
+            }
+        }
     }
     onSubmit() {
-        const data = this.invoiceForm.value;
-        const client_id = localStorage.getItem('client_id');
-        const invoiceModelData = {
-            'id': client_id,
-            'invoiceFooter': {
-                'address': {
-                    'address1': data.addressLine1,
-                    'address2': data.addressLine2,
-                    'city': data.city,
-                    'state': data.state,
-                    'zipCode': data.zipcode
+        this.submitted = true;
+        if (this.invoiceForm.valid && this.submitted) {
+            const data = this.invoiceForm.value;
+            const client_id = localStorage.getItem('client_id');
+            const invoiceModelData = {
+                'id': client_id,
+                'invoiceFooter': {
+                    'address': {
+                        'address1': data.addressLine1,
+                        'address2': data.addressLine2,
+                        'city': data.city,
+                        'state': data.state,
+                        'zipCode': data.zipcode
+                    },
+                    'id': client_id,
+                    'termsCondition': data.terms
                 },
-                'id': client_id,
-                'termsCondition': data.terms
-            },
-            'invoiceHeader': {
-                'id': client_id,
-                'logo': data.logo
-            },
-            'userId': client_id
-        };
-        this.masterService.createInvoice(invoiceModelData).subscribe(
-            result => {
-                console.log(result);
-                // this.revert();
-            },
-            err => {
-                console.log(err.error);
-            }
-        );
+                'invoiceHeader': {
+                    'id': client_id,
+                    'logo': data.logo
+                },
+                'userId': client_id
+            };
+            this.masterService.createInvoice(invoiceModelData).subscribe(
+                result => {
+                    this.revert();
+                },
+                err => {
+                    console.log(err.error);
+                }
+            );
+        }
     }
     revert() {
-        this.invoiceForm.reset();
-        this.photoUrl = '';
+        this.submitted = false;
+        this.getTemplateData();
+        this.editTemplate = false;
+        this.createForm();
     }
     onFileChange(event) {
         const reader = new FileReader();
@@ -90,11 +110,11 @@ export class InvoiceInfoComponent implements OnInit {
             reader.onload = (e: any) => {
                 this.photoUrl = e.target.result;
                 this.invoiceForm.get('logo').setValue(reader.result.split(',')[1]);
-                // this.photoUrl = event.target.result;
             };
         }
     }
     editInvoice() {
         this.editTemplate = true;
+        this.createForm();
     }
 }
