fcda7e7c44a3fc6215b862d761532d17d3635927
vaibhav.joshi <vaibhav.joshi@globallogic.com>
2018-08-20 10:45:19 +0530

    User Detail Component update

--- src/app/feature-shell/systemdashboard/userdetail/userdetail.component.ts
+++ src/app/feature-shell/systemdashboard/userdetail/userdetail.component.ts
@@ -25,18 +25,12 @@ export class UserdetailComponent implements OnInit {
   isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('detailRow');
   expandedElement: any;
 
-  helpers = Chart.helpers;
   title : any
    service: any;
    userList: User[];
    moreIndex:number;
   constructor(private _systemdashService: SystemdashboardService, private _activatedRoute: ActivatedRoute
   ,private _userService: UserService) { }
-  displayedColumns: any;
-  dataSource: any;
-  
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   /**
    * Set the paginator after the view init since this component will
@@ -46,9 +40,6 @@ export class UserdetailComponent implements OnInit {
 
   ngOnInit() {
 
-    this.displayedColumns = ['name', 'email','organization', ];
-    this.dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
-   
     this._activatedRoute.params.subscribe((params: Params) => {
        this.service= params.mode;
       });
@@ -71,10 +62,6 @@ export class UserdetailComponent implements OnInit {
           result => {
             this.userList = result;
             this.title = 'Total Customers';
-            //this.dataSource = new MatTableDataSource<any>(this.userList);
-            //this.dataSource = result;
-            this.dataSource.paginator = this.paginator;
-            console.log(this.isExpansionDetailRow);
           });
         }
   }
@@ -85,32 +72,3 @@ export class UserdetailComponent implements OnInit {
   }
 }
 
-export interface Element {
-  name: string;
-  position: number;
-  organization: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA= [
-  {position: 1, name: 'Hydrogen', organization: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', organization: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', organization: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', organization: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', organization: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', organization: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', organization: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', organization: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', organization: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', organization: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', organization: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', organization: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', organization: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', organization: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', organization: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', organization: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', organization: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', organization: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', organization: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', organization: 40.078, symbol: 'Ca'},
-];
--- src/app/shared/shared-config.ts
+++ src/app/shared/shared-config.ts
@@ -1,1 +1,1 @@
-export let endpoint_url = 'http://localhost:7776';
+export let endpoint_url = 'http://13.127.27.1:7777';
